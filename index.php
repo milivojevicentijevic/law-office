366d901b36a4a2bbc0c69791237a9ee83ff5e04f
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-17 21:08:47 +0100

    testimonials css

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
     <!-- consultation -->
     <section class="consultation">
         <article>
-            <h2>Get a free no obligation conslutation</h2>
+            <h2>Get a free no <br> obligation <br> conslutation</h2>
         </article>
         <article>
             <form>
