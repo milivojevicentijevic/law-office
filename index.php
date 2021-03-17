999699a2eaaf192d4235da46c500448adac00326
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-17 15:58:47 +0100

    about html

--- index.php
+++ index.php
@@ -2,30 +2,41 @@
 
     <!-- services -->
     <section class="services container">
-    <article>
-        <i class="fas fa-landmark"></i>
-        <h3>Family law</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
-        <button>Learn more</button>
-    </article>
-    <article>
-        <i class="fas fa-landmark"></i>
-        <h3>Business law</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
-        <button>Learn more</button>
-    </article>
-    <article>
-        <i class="fas fa-landmark"></i>
-        <h3>Criminal law</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
-        <button>Learn more</button>
-    </article>
-    <article>
-        <i class="fas fa-landmark"></i>
-        <h3>Insurance law</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
-        <button>Learn more</button>
-    </article>
-</section>  
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Family law</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Business law</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Criminal law</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+            <button>Learn more</button>
+        </article>
+        <article>
+            <i class="fas fa-landmark"></i>
+            <h3>Insurance law</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam repellat laudantium perspiciatis, facilis, fuga tenetur consequuntur officia. Numquam minima beatae adipisci blanditiis suscipit praesentium cum ipsa itaque nesciunt aperiam non eius accusantium animi nulla, voluptatem inventore ut!</p>
+            <button>Learn more</button>
+        </article>
+    </section>  
+    <!-- about  -->
+    <section class="about">
+        <article>
+            <h2>About us</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam vel cum nulla mollitia earum ad maxime laudantium ipsa cupiditate totam nostrum vero, reiciendis consequatur sunt voluptates laborum, ducimus repellendus possimus at autem deleniti! Facilis est vero cum consequuntur aliquam nobis!</p>
+            <a href="">Learn more &nbsp; &rang;</a>
+        </article>
+        <article>
+            <h2>Welcome to <br> <span>Solo Practice</span> <br> Law</h2>
+        </article>
+    </section>
 
 <?php include 'partials/footer.php' ?>
