88a08d84a91e6f409e18144b02fcb49522e600ff
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-18 12:17:52 +0100

    crew

--- about.php
+++ about.php
@@ -1,5 +1,45 @@
 <?php include 'partials/main-header.php' ?>
 
-    <h1 style="text-align:center;font-size:10rem;">About</h1>
+    <!-- crew -->
+    <section class="crew">
+        <div class="container">
+            <article>
+                <img src="img/people1.jpg" alt="">
+                <div class="text">
+                    <h3>Etta James</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
+                    <button>Learn more</button>
+                    <button>Contact</button>
+                </div>
+            </article>
+            <article>
+                <img src="img/people2.jpg" alt="">
+                <div class="text">
+                    <h3>Louis Armstrong</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
+                    <button>Learn more</button>
+                    <button>Contact</button>
+                </div>
+            </article>
+            <article>
+                <img src="img/people3.jpg" alt="">
+                <div class="text">
+                    <h3>Bessie Smith</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
+                    <button>Learn more</button>
+                    <button>Contact</button>
+                </div>
+            </article>
+            <article>
+                <img src="img/people4.jpg" alt="">
+                <div class="text">
+                    <h3>Elmore James</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, delectus saepe illo labore, porro harum alias qui soluta iusto laborum amet corporis sit quo adipisci! A beatae at esse, tempore.</p>
+                    <button>Learn more</button>
+                    <button>Contact</button>
+                </div>
+            </article>
+        </div>
+    </section>
 
 <?php include 'partials/footer.php' ?>
