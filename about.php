bd2b79ebe47188b7638297a9311dca1d1428f235
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-18 12:26:47 +0100

    accidents

--- about.php
+++ about.php
@@ -41,5 +41,44 @@
             </article>
         </div>
     </section>
+    <!-- accidents -->
+    <section class="accidents">
+        <h2>Type of pesonal injury and accident cades we handle</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, nulla harum laudantium quos, fugit nemo porro quis blanditiis odit ullam quia nesciunt molestiae culpa deleniti!</p>
+        <article class="container">
+            <div>
+                <h3>Personal injury</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quis libero veritatis modi, totam similique excepturi ipsum atque sint debitis deleniti accusantium et vitae nisi natus quidem laborum aliquid aspernatur?</p>
+            </div>
+            <div>
+                <h3>Wrongfull death</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quis libero veritatis modi, totam similique excepturi ipsum atque sint debitis deleniti accusantium et vitae nisi natus quidem laborum aliquid aspernatur?</p>
+            </div>
+            <div>
+                <h3>Medical malpractice</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quis libero veritatis modi, totam similique excepturi ipsum atque sint debitis deleniti accusantium et vitae nisi natus quidem laborum aliquid aspernatur?</p>
+            </div>
+            <div>
+                <h3>Premises liability</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quis libero veritatis modi, totam similique excepturi ipsum atque sint debitis deleniti accusantium et vitae nisi natus quidem laborum aliquid aspernatur?</p>
+            </div>
+            <div>
+                <h3>Auto accidents</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quis libero veritatis modi, totam similique excepturi ipsum atque sint debitis deleniti accusantium et vitae nisi natus quidem laborum aliquid aspernatur?</p>
+            </div>
+            <div>
+                <h3>Track accidents</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quis libero veritatis modi, totam similique excepturi ipsum atque sint debitis deleniti accusantium et vitae nisi natus quidem laborum aliquid aspernatur?</p>
+            </div>
+            <div>
+                <h3>Unisured motorists</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quis libero veritatis modi, totam similique excepturi ipsum atque sint debitis deleniti accusantium et vitae nisi natus quidem laborum aliquid aspernatur?</p>
+            </div>
+            <div>
+                <h3>Motorcycle accidents</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quis libero veritatis modi, totam similique excepturi ipsum atque sint debitis deleniti accusantium et vitae nisi natus quidem laborum aliquid aspernatur?</p>
+            </div>
+        </article>
+    </section>
 
 <?php include 'partials/footer.php' ?>
