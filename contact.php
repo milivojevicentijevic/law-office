80d46313197d92f3fd5dee62bb75dc9329b3a9a1
milivojevicentijevic <milivojevicentijevic985@gmail.com>
2021-03-18 13:17:37 +0100

    consultate

--- contact.php
+++ contact.php
@@ -1,5 +1,19 @@
 <?php include 'partials/main-header.php' ?>
 
-    <h1 style="text-align:center;font-size:10rem;">Contact</h1>
+    <!-- consultate -->
+    <section class="consultate">
+        <article>
+            <h2>Get a free no <br> obligation <br> conslutation</h2>
+        </article>
+        <article>
+            <form>
+                <input type="text" placeholder="First name">
+                <input type="text" placeholder="Last name">
+                <input type="email" placeholder="Email">
+                <textarea cols="30" rows="10" placeholder="Message"></textarea>
+                <button>Submit</button>
+            </form>
+        </article>
+    </section>
 
 <?php include 'partials/footer.php' ?>
